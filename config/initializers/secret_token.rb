c51fcf6c576027dcf3bc062263ed6eeb2eb4a94e
Bojan <htbuffalo@gmail.com>
2012-04-20 16:34:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bojan::Application.config.secret_token = '208e4db921ca94dbe990913e01fc28870a6695903d9e0f2c26b2dc779d63ba96f2dfb6b396b7d80b437891039b77f5543c73ec718f6e19e098eae698e8e0162f'
